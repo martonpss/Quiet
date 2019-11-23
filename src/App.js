145d19f5f0bebcaad06fae50f14147fe30a1b3e9
martonpss <martonpss@gmail.com>
2019-11-23 14:38:31 -0300

    Configura página inicial

--- src/App.js
+++ src/App.js
@@ -5,9 +5,23 @@ import logo from './assets/logo.svg';
 
 function App() {
   return (
+
     <div className="container">
+      
       <img src={logo} alt="Logo da quiet" />
+
+      <div className="content">
+        <p>
+          Nossa missão é <strong>reduzir</strong> os conflitos causados pela <strong>poluição sonora</strong>
+        </p>
+
+        <button className="btn" id="btnDenunciar">Denunciar</button>
+        <button className="btn" id="btnRegistrar">Registrar evento</button>
+      </div>
+
+
     </div>
+
   );
 }
 
