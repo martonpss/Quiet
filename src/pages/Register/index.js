abf6459bbfdfe52ee2608b31f6e1ff3f75ffa411
martonpss <martonpss@gmail.com>
2019-11-23 18:32:29 -0300

    Configura página de registro

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,62 @@
 import React from 'react';
 
 export default function Register() {
-	return <div />
+	
+	function handleSubmit(event) {
+		event.preventDefault();
+
+		/*
+			ATENÇÃO!
+			A geolocalização deve ser enviada.
+		*/
+
+	}
+
+	
+	return (
+		<>
+			<p>
+				Qual é sua queixa?
+			</p>
+			<form onSubmit={handleSubmit}>
+
+				<div className="registerType">
+					<label htmlFor="registerType">TIPO DO EVENTO</label>
+					<select>
+						<option selected value="public">Público</option>
+						<option value="private">Privado</option>
+					</select>
+				</div>
+
+				<label htmlFor="registerLink">LINK</label>
+				<input
+					type="text"
+					id="registerLink"
+					placeholder="URL do evento"
+				/>
+
+				<label htmlFor="registerTitle">TÍTULO *</label>
+				<input
+					type="text"
+					id="registerTitle"
+					placeholder="Descrição breve"
+				/>
+
+				<label htmlFor="registerDescription">DESCRIÇÃO</label>
+				<textarea
+					id="registerDescription"
+					placeholder="Descrição detalhada"
+				/>
+
+				<label htmlFor="registerContact">CONTATO *</label>
+				<input
+					type="text"
+					id="registerContact"
+					placeholder="Número do Telegram"
+				/>
+
+				<button type="submit" className="btn">Enviar</button>
+			</form>
+		</>
+	);
 }
