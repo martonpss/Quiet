fcac749b755c762277b62eed641ad57cea0a616b
martonpss <martonpss@gmail.com>
2019-11-23 13:58:36 -0300

    Adiciona logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <h1>Hello, World!</h1>
+    <div className="container">
+      <img src={logo} alt="Logo da quiet" />
+    </div>
   );
 }
 
