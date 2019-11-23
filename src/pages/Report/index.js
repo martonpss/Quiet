a2797859090839eeea675ae4c42b7d6adbcda4fb
martonpss <martonpss@gmail.com>
2019-11-23 17:59:15 -0300

    Configura página de denúncia

--- src/pages/Report/index.js
+++ src/pages/Report/index.js
@@ -1,5 +1,40 @@
 import React from 'react';
 
 export default function Report() {
-	return <div />
+	
+	function handleSubmit(event) {
+		event.preventDefault();
+
+		/*
+			ATENÇÃO!
+			Os valores passados devem ser o título, descrição e geolocalização.
+			O usuário só pode fazer a submissão a cada certo intervalo de tempo.
+		*/
+
+	}
+
+	
+	return (
+		<>
+			<p>
+				Qual é sua queixa?
+			</p>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="reportTitle">TÍTULO *</label>
+				<input
+					type="text"
+					id="reportTitle"
+					placeholder="Descrição breve"
+				/>
+
+				<label htmlFor="reportDescription">DESCRIÇÃO</label>
+				<textarea
+					id="reportDescription"
+					placeholder="Descrição detalhada"
+				/>
+
+				<button type="submit" className="btn">Enviar</button>
+			</form>
+		</>
+	);
 }
